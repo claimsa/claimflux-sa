b2c4126fe142d3446692b36e7e31d8c9a94b1f76
claimsa <tosintoday26@gmail.com>
2026-06-22 23:48:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask
+from config import Config
+from extensions import limiter
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+limiter.init_app(app)
+
+from routes.pages import pages_bp
+from routes.api import api_bp
+
+app.register_blueprint(pages_bp)
+app.register_blueprint(api_bp)
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
