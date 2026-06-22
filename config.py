5bd2e11fef63d5b697ae2c1ff7227ed90225ed14
claimsa <tosintoday26@gmail.com>
2026-06-22 23:44:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,20 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "change-this-in-production"
+    )
+
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_SERVICE_ROLE = os.getenv("SUPABASE_SERVICE_ROLE")
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    GMAIL_USER = os.getenv("GMAIL_USER")
+    GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+
+    DEFAULT_CPA_DAYS = 182
