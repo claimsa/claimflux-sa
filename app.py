2e57377597583118f572c20b22db45219440bf29
claimsa <tosintoday26@gmail.com>
2026-06-24 17:55:36 +0200

    Update app.py345

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app.config.from_object(Config)
 limiter.init_app(app)
 
 from routes.pages import pages_bp
+from routes.api import api_bp
 
 app.register_blueprint(pages_bp)
-
+app.register_blueprint(api_bp)
 
 if __name__ == "__main__":
     app.run()
