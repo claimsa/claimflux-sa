f8a32403247b76e9eddb0b3c358a15acb9c5b798
claimsa <tosintoday26@gmail.com>
2026-06-24 16:44:25 +0200

    Update app.py123

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "ClaimFlux SA is alive"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
