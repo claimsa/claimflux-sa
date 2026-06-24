6fa868d4f9e182e4347e65733377403e5bcad001
claimsa <tosintoday26@gmail.com>
2026-06-24 16:36:56 +0200

    Update app.py12

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask
-from config import Config
-from extensions import limiter
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
-limiter.init_app(app)
-
-from routes.pages import pages_bp
-from routes.api import api_bp
-
-app.register_blueprint(pages_bp)
-app.register_blueprint(api_bp)
+@app.route("/")
+def home():
+    return "ClaimFlux SA is alive"
 
 if __name__ == "__main__":
     app.run()
