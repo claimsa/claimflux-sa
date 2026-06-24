7974dd6ff79b346666621bb77a5d60e033f5e2e5
claimsa <tosintoday26@gmail.com>
2026-06-24 10:31:50 +0200

    Update pages.py333

--- routes/pages.py
+++ routes/pages.py
@@ -5,47 +5,53 @@ from services.auth_service import set_auth_token
 
 pages_bp = Blueprint("pages", __name__)
 
+
 @pages_bp.route("/")
 def home():
     return render_template("index.html")
+
 
 @pages_bp.route("/scan")
 def scan():
     return render_template("scan.html")
 
+
 @pages_bp.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
+
 @pages_bp.route("/verify-login/<token>")
 def verify_login(token):
 
-response = (
-    supabase.table("login_tokens")
-    .select("*")
-    .eq("token", token)
-    .eq("used", False)
-    .execute()
-)
-
-if response.data:
-
-    email = response.data[0]["email"]
-
-    (
+    response = (
         supabase.table("login_tokens")
-        .update({"used": True})
+        .select("*")
         .eq("token", token)
+        .eq("used", False)
         .execute()
     )
 
-    resp = redirect(f"/dashboard?email={email}")
+    if response.data:
 
-    resp = set_auth_token(
-        email,
-        resp
-    )
+        email = response.data[0]["email"]
 
-    return resp
+        (
+            supabase.table("login_tokens")
+            .update({"used": True})
+            .eq("token", token)
+            .execute()
+        )
 
-return "Invalid or expired link", 401
+        resp = redirect(
+            f"/dashboard?email={email}"
+        )
+
+        resp = set_auth_token(
+            email,
+            resp
+        )
+
+        return resp
+
+    return "Invalid or expired link", 401
